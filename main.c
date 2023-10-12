f768e57dfe956400b92f4ed4a2cf9bf53144ae64
Eugenio Caruso <ecaruso@e4r6p4.42roma.it>
2023-10-12 18:52:40 +0200

    hello

--- main.c
+++ main.c
@@ -6,15 +6,47 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:23:39 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/10/11 17:46:36 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/10/12 18:28:37 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosopher.h"
+
+int	ft_atoi(const char *str)
+{
+	int	neg;
+	int	num;
+	int	i;
+
+	neg = 1;
+	num = 0;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10;
+		num += (str[i] - 48);
+		i++;
+	}
+	num *= neg;
+	return (num);
+}
+
 int main(int argc, char **argv)
 {
+	t_philo	philo;
+	
 	if (argc < 5 || argc > 6)
 	{
 		printf("ERROR:argoument count not valid");
 		return (0);
 	}
+	init(&philo, argc, argv);
 }
--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:24:48 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/10/11 18:07:17 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/10/12 17:37:01 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,20 @@
 # define THINK "is thinking"
 # define DIE "died"
 
+# include <stdio.h>
+# include <pthread.h>
 
 typedef struct s_philo
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	max_eat;
+	pthread_t		thread;
+	int				id;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				max_eat;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
 }t_philo;
 
 #endif
